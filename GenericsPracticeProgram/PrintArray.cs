ecc67d2830933b455fe25b51f092ff5bba23058f
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 23:49:32 +0530

    [Vishwas]add: Printing Simple Array Program

---
+++ GenericsPracticeProgram/PrintArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProgram
+{
+    public class PrintArray
+    {
+        public static void Toprint(int[] inputArray)
+        {
+            foreach(var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------");
+        }
+        public static void Toprint(double[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------");
+        }
+        public static void Toprint(char[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
--- GenericsPracticeProgram/Program.cs
+++ GenericsPracticeProgram/Program.cs
@@ -6,6 +6,13 @@ namespace GenericsPracticeProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Practice Program of Generics....");
+            int[] intArray = { 11, 22, 33, 44, 55, 66};
+            double[] doubleArray = { 11.22, 33.44, 55.66, 77.88 };
+            char[] charArray = { 'I', 'N', 'D', 'I', 'A' };
+
+            PrintArray.Toprint(intArray);
+            PrintArray.Toprint(doubleArray);
+            PrintArray.Toprint(charArray);
         }
     }
 }
