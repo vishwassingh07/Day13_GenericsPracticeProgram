9d49fac2e30dd48d1076192ba7300ea08945cfc5
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 23:37:23 +0530

    [Vishwas]add: Generics Practice Program Added

---
+++ GenericsPracticeProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GenericsPracticeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Practice Program of Generics....");
+        }
+    }
+}
