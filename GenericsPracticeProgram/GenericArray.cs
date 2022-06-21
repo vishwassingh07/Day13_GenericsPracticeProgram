42fcdc91b65be87fc5cf232737433194c5f54611
Vishwas <baghel.vishwas0@gmail.com>
2022-06-21 11:58:25 +0530

    [Vishwas]add: Printing Array Using Generics Class

---
+++ GenericsPracticeProgram/GenericArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProgram
+{
+    public class GenericArray<T>
+    {
+        public T[] inputarray;
+        public GenericArray(T[] inputArray)
+        {
+            this.inputarray = inputArray;
+        }
+        public void ToPrint()
+        {
+            foreach (var element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
--- GenericsPracticeProgram/Program.cs
+++ GenericsPracticeProgram/Program.cs
@@ -10,6 +10,10 @@ namespace GenericsPracticeProgram
             double[] doubleArray = { 11.22, 33.44, 55.66, 77.88 };
             char[] charArray = { 'I', 'N', 'D', 'I', 'A' };
 
+            new GenericArray<int>(intArray).ToPrint();
+            new GenericArray<double>(doubleArray).ToPrint();
+            new GenericArray<char>(charArray).ToPrint();
+
             PrintArray.Toprint<int>(intArray);
             PrintArray.Toprint<double>(doubleArray);
             PrintArray.Toprint<char>(charArray);
